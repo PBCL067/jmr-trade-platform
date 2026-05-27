5f0221f6ef8af5409173c58eca626b11a773b0f1
Matt Stevens <mcallcotts@gmail.com>
2026-05-27 17:52:15 -0300

    Add INDEMIL Brazil (modified starch manufacturer) and Barentz SA (buyer-side distributor)

--- src/SupplierMap.js
+++ src/SupplierMap.js
@@ -3,6 +3,8 @@ import { SUPPLIERS } from './data/supplierData';
 
 const SUPPLIER_COORDS = {
   "horizonte_amidos_brazil":  { lat: -24.5585,   lng: -54.0553   },
+  "indemil_brazil":           { lat: -15.7801,   lng: -47.9292   },
+  "barentz_south_africa":     { lat: -26.2041,   lng:  28.0473   },
   "ingredion_argentina":      { lat: -33.802183, lng: -59.504723 },
   "agd_argentina":             { lat: -32.7538,   lng: -63.7881   },
   "molinos_rio_argentina":     { lat: -34.4692,   lng: -58.5652   },
